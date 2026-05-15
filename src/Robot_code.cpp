573f5de72652a71caaf38d4ef541a80b8fdf9b34
Zhanuzak Sayassat <sayassat.zhanuzak@gmail.com>
2026-05-16 00:14:01 +0500

    Report modifications

--- src/Robot_code.cpp
+++ src/Robot_code.cpp
@@ -93,7 +93,7 @@ void loop()
   Serial.println(distance);
 
   // obstcle avoidance algorithm
-  if (distance > 0 && distance < OBS_DISTANCE)
+if (distance > 0 && distance < OBS_DISTANCE)
   {
     left_motor(-v_0);
     right_motor(-v_0);
